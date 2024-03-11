bee91d61261fa9fbd15d6ea07ce75c01e26bef23
Kenny-ai <kennysoyoye07@gmail.com>
2024-03-11 02:48:50 +0100

    added loading component

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,19 +1,23 @@
 import React from "react";
+import Loading from "./Loading";
 interface Props {
   title: string;
   className: string;
   onClick?: () => void;
   type?: "submit" | "reset" | "button";
+  isLoading?: boolean;
 }
 
-const Button = ({ title, className, onClick, type }: Props) => {
+const Button = ({ title, className, onClick, type, isLoading }: Props) => {
   return (
     <button
-      className={`${className} w-40 py-4 font-bold rounded-lg uppercase duration-200`}
+      className={`${className} w-40 py-4 font-bold rounded-lg uppercase duration-200 ${
+        isLoading ? `grid place-items-center` : ``
+      } `}
       onClick={onClick}
       type={type}
     >
-      {title}
+      {isLoading ? <Loading /> : title}
     </button>
   );
 };
