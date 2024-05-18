be6c1456c90c80b25f1edef62714e5a9211167d8
Kenny-ai <kennysoyoye07@gmail.com>
2024-05-19 00:58:25 +0100

    fix auth validation error msg

--- src/utils/validation/auth.ts
+++ src/utils/validation/auth.ts
@@ -1,7 +1,9 @@
 import * as Yup from "yup";
 
 export const loginSchema = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Please enter a valid email address"),
   password: Yup.string()
     .min(5, "Password must be 5 characters or more")
     .required("Required"),
@@ -10,18 +12,11 @@ export const loginSchema = Yup.object({
 export const registerSchema = Yup.object({
   name: Yup.string()
     .max(20, "Name must be 20 characters or less")
-    .required("Required"),
-  email: Yup.string().email("Invalid email address").required("Required"),
+    .required("Please enter a valid name"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Please enter a valid email address"),
   password: Yup.string()
     .min(5, "Password must be 5 characters or less")
     .required("Required"),
-});
-
-export const checkoutSchema = Yup.object({
-  name: Yup.string()
-    .max(20, "Name must be 20 characters or less")
-    .required("Required"),
-  email: Yup.string().email("Invalid email address").required("Required"),
-  phone: Yup.string().email("Invalid email address").required("Required"),
-  address: Yup.string().required("Required"),
 });
